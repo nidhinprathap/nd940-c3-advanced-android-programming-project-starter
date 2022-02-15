94e43ee3c03d14ec9bed2bbcb2cc3fdcd8ddd5a9
Nidhin <nidhin@trellisys.net>
2022-02-15 16:41:12 +0530

    rewrote the loading button

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,184 +1,169 @@
 package com.udacity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.*
-import android.os.Build
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.content_main.view.*
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
+
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    private var widthSize = 0
-    private var heightSize = 0
-    private var buttonText: String
-    private var buttonBackgroundColor = R.attr.buttonBackgroundColor
-    private var progress: Float = 0f
+    private var btnWidthSize = 0
+    private var btnHeightSize = 0
+    private var lblWidth = 0f
+
+    private var lblTextSize: Float = resources.getDimension(R.dimen.default_text_size)
+    private var circleXOffset = lblTextSize / 2
+
+    private lateinit var buttonTitle: String
+
+    private var progressWidth = 0f
+    private var progressCircle = 0f
+
+    private var buttonDefaultColor = ContextCompat.getColor(context, R.color.colorPrimary)
+    private var btnProgressColor = ContextCompat.getColor(context, R.color.colorPrimaryDark)
+    private var progressBarColor = ContextCompat.getColor(context, R.color.colorAccent)
+
     private var valueAnimator = ValueAnimator()
-    private val textRect = Rect()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { property, oldValue, newValue ->
-        when (newValue) {
-            ButtonState.Loading -> {
-                setText("We are Downloading")
-                setBgColor("#004349")
-                valueAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
-                    addUpdateListener {
-                        progress = animatedValue as Float
-                        invalidate()
-                    }
-                    repeatMode = ValueAnimator.REVERSE
-                    repeatCount = ValueAnimator.INFINITE
-                    duration = 3000
-                    start()
-                }
-                disableLoadingButton()
-            }
-
-            ButtonState.Completed -> {
-                setText("Downloaded")
-                setBgColor("#07C2AA")
-                valueAnimator.cancel()
-                resetProgress()
-                enableLoadingButton()
-            }
-
+    private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { p, old, new ->
+        when (new) {
             ButtonState.Clicked -> {
+                setButtonText(context.getString(R.string.clicked))
+                invalidate()
+            }
+            ButtonState.Loading -> {
+                setButtonText(resources.getString(R.string.button_loading))
+                startDownloadAnimation()
+
+            }
+            ButtonState.Completed -> {
+                setButtonText(resources.getString(R.string.button_download))
+                onDownloadComplete()
+                invalidate()
             }
         }
-        invalidate()
+
     }
 
-    init {
-        context.theme.obtainStyledAttributes(
-            attrs,
-            R.styleable.LoadingButton,
-            0, 0
-        ).apply {
+    private fun onDownloadComplete() {
+        valueAnimator.cancel()
+        progressWidth = 0f
+        progressCircle = 0f
+    }
 
-            try {
-                buttonText = getString(R.styleable.LoadingButton_text).toString()
-                buttonBackgroundColor = ContextCompat.getColor(context, R.color.colorPrimary)
-            } finally {
-                recycle()
-            }
+    private fun startDownloadAnimation() {
+        valueAnimator = ValueAnimator.ofFloat(0f, btnWidthSize.toFloat())
+        valueAnimator.duration = 5000
+        valueAnimator.addUpdateListener { animation ->
+            progressWidth = animation.animatedValue as Float
+            progressCircle = (btnWidthSize.toFloat() / 365) * progressWidth
+            invalidate()
         }
+        valueAnimator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                progressWidth = 0f
+                if (buttonState == ButtonState.Loading) {
+                    buttonState = ButtonState.Loading
+                }
+            }
+        })
+        valueAnimator.start()
     }
 
-    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        textSize = 50.0f
-        color = Color.WHITE
+    private val paint = Paint().apply {
+        isAntiAlias = true
+        textSize = resources.getDimension(R.dimen.default_text_size)
     }
 
-    private val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = ContextCompat.getColor(context, R.color.colorPrimary)
-    }
 
-    private val inProgressBackgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = ContextCompat.getColor(context, R.color.colorPrimaryDark)
-    }
-
-    private val inProgressArcPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.YELLOW
-    }
-
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    override fun onDraw(canvas: Canvas) {
+    override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        val cornerRadius = 8.0f
-        val backgroundWidth = measuredWidth.toFloat()
-        val backgroundHeight = measuredHeight.toFloat()
+        // UPDATE BUTTON COLOR
+        setButtonBGColor(canvas)
+        //UPDATE BUTTON PROGRESS
+        setProgressOnButton(canvas)
+        //UPDATE PROGRESS
+        updateProgress(canvas)
+        // ADD TEXT TO BUTTON
+        drawTextOnButton(canvas)
+    }
 
-        canvas.drawColor(buttonBackgroundColor)
-        textPaint.getTextBounds(buttonText, 0, buttonText.length, textRect)
-        canvas.drawRoundRect(
-            0f,
-            0f,
-            backgroundWidth,
-            backgroundHeight,
-            cornerRadius,
-            cornerRadius,
-            backgroundPaint
+    private fun setButtonBGColor(canvas: Canvas?) {
+        paint.color = buttonDefaultColor
+        canvas?.drawRect(0f, 0f, btnWidthSize.toFloat(), btnHeightSize.toFloat(), paint)
+    }
+
+    private fun setProgressOnButton(canvas: Canvas?) {
+        paint.color = btnProgressColor
+        canvas?.drawRect(0f, 0f, progressWidth, btnHeightSize.toFloat(), paint)
+    }
+
+    private fun updateProgress(canvas: Canvas?) {
+        canvas?.save()
+        canvas?.translate(
+            btnWidthSize / 2 + lblWidth / 2 + circleXOffset,
+            btnHeightSize / 2 - lblTextSize / 2
         )
-
-        if (buttonState == ButtonState.Loading) {
-            var progressVal = progress * measuredWidth.toFloat()
-            canvas.drawRoundRect(
-                0f,
-                0f,
-                progressVal,
-                backgroundHeight,
-                cornerRadius,
-                cornerRadius,
-                inProgressBackgroundPaint
-            )
-
-            val arcDiameter = cornerRadius * 2
-            val arcRectSize = measuredHeight.toFloat() - paddingBottom.toFloat() - arcDiameter
-
-            progressVal = progress * 360f
-            canvas.drawArc(
-                paddingStart + arcDiameter,
-                paddingTop.toFloat() + arcDiameter,
-                arcRectSize,
-                arcRectSize,
-                0f,
-                progressVal,
-                true,
-                inProgressArcPaint
-            )
-        }
-        val centerX = measuredWidth.toFloat() / 2
-        val centerY = measuredHeight.toFloat() / 2 - textRect.centerY()
-
-        canvas.drawText(buttonText, centerX, centerY, textPaint)
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val minWidth: Int = paddingLeft + paddingRight + suggestedMinimumWidth
-        val width: Int = resolveSizeAndState(minWidth, widthMeasureSpec, 1)
-        val height: Int = resolveSizeAndState(
-            MeasureSpec.getSize(width),
-            heightMeasureSpec,
-            0
+        paint.color = progressBarColor
+        canvas?.drawArc(
+            RectF(0f, 0f, lblTextSize, lblTextSize),
+            0F,
+            progressCircle * 0.365f,
+            true,
+            paint
         )
-        widthSize = width
-        heightSize = height
-        setMeasuredDimension(width, height)
+        canvas?.restore()
     }
 
-    private fun disableLoadingButton() {
-        loading_button.isEnabled = false
+    private fun drawTextOnButton(canvas: Canvas?) {
+        paint.color = Color.WHITE
+        lblWidth = paint.measureText(buttonTitle)
+        canvas?.drawText(
+            buttonTitle,
+            btnWidthSize / 2 - lblWidth / 2,
+            btnHeightSize / 2 - (paint.descent() + paint.ascent()) / 2,
+            paint
+        )
     }
 
-    private fun enableLoadingButton() {
-        loading_button.isEnabled = true
+    private fun setButtonText(text: String) {
+        buttonTitle = text
     }
 
     fun setLoadingButtonState(state: ButtonState) {
         buttonState = state
     }
 
-    private fun setText(buttonText: String) {
-        this.buttonText = buttonText
-        invalidate()
-        requestLayout()
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minWidth: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+        btnWidthSize = resolveSizeAndState(minWidth, widthMeasureSpec, 1)
+        btnHeightSize = resolveSizeAndState(
+            MeasureSpec.getSize(btnWidthSize),
+            heightMeasureSpec,
+            0
+        )
+        setMeasuredDimension(btnWidthSize, btnHeightSize)
     }
 
-    private fun setBgColor(backgroundColor: String) {
-        buttonBackgroundColor = Color.parseColor(backgroundColor)
-        invalidate()
-        requestLayout()
+    init {
+        setButtonText(resources.getString(R.string.button_download))
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            // SETTING DEFAULT VALUES TO STYLE ATTRIBUTES
+            buttonDefaultColor = getColor(R.styleable.LoadingButton_buttonDefaultColor, 0)
+            btnProgressColor = getColor(R.styleable.LoadingButton_btnProgressColor, 0)
+            progressBarColor = getColor(R.styleable.LoadingButton_progressBarColor, 0)
+        }
     }
 
-    private fun resetProgress() {
-        progress = 0f
-    }
 }
